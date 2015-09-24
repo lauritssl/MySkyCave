c98ff7593038f6c2e10ef527e1292f1d66363aad
Søren Lundtoft <soren.lundtoft@gmail.com>
2015-09-24 13:07:41 +0200

    Unused imports and variables removed

--- src/cloud/cave/server/StatelessSessionCache.java
+++ src/cloud/cave/server/StatelessSessionCache.java
@@ -1,29 +1,14 @@
 package cloud.cave.server;
 
 import cloud.cave.domain.Player;
-import cloud.cave.domain.Region;
-import cloud.cave.server.common.PlayerRecord;
-import cloud.cave.server.common.RoomRecord;
-import cloud.cave.server.common.ServerConfiguration;
-import cloud.cave.server.common.SubscriptionRecord;
 import cloud.cave.service.CaveStorage;
 import cloud.cave.service.WeatherService;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-
-import java.util.Arrays;
-
-import static com.mongodb.client.model.Filters.eq;
 
 /**
  * Created by Soren and Laurits on 23/09/15.
  */
 public class StatelessSessionCache implements PlayerSessionCache {
-    private MongoCollection<Document> cache;
+
     private final CaveStorage storage;
     private final WeatherService weatherService;
     private final PlayerSessionCache playerSessionCache;
@@ -32,21 +17,6 @@ public class StatelessSessionCache implements PlayerSessionCache {
         this.storage = storage;
         this.weatherService = weatherService;
         this.playerSessionCache = playerSessionCache;
-<<<<<<< Updated upstream
-
-        ServerConfiguration config = storage.getConfiguration();
-
-        ServerAddress[] mongoServers = new ServerAddress[config.size()];
-
-        for (int i = 0; i < config.size(); i++) {
-            mongoServers[i] = new ServerAddress(config.get(i).getHostName(), config.get(i).getPortNumber());
-        }
-
-        MongoClient mongo = new MongoClient(Arrays.asList(mongoServers));
-        MongoDatabase db = mongo.getDatabase("cave");
-        cache = db.getCollection("cache");
-=======
->>>>>>> Stashed changes
     }
 
     @Override
