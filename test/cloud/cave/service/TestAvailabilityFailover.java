4a0f37261c2071be2fe7967fe5cd705da96802bd
Søren Lundtoft <soren.lundtoft@gmail.com>
2015-10-06 15:23:58 +0200

    Location listner removed on pause

--- test/cloud/cave/service/TestAvailabilityFailover.java
+++ test/cloud/cave/service/TestAvailabilityFailover.java
@@ -294,6 +294,6 @@ public class TestAvailabilityFailover {
         }
 
         assertEquals("Should have thrown a CaveStorageException", CaveStorageException.class, exception.getClass());
-        
+
     }
 }
